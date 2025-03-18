9a0d87339fbad4b108d7e5364de6c6d829e74a82
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-03-18 19:56:41 +0100

    Delete .eslintrc.js

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['react-app', 'react-app/jest'],
-  rules: {
-    // Change no-unused-vars from error to warning to prevent build failures
-    'no-unused-vars': 'warn',
-  }
-};
