f8a3a079012f6e60b6b3485a3c5f564b6d424e8c
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-03-19 13:12:18 +0100

    Update FermiPokerIntro.js

--- src/components/FermiPokerIntro.js
+++ src/components/FermiPokerIntro.js
@@ -20,7 +20,7 @@ const FermiPokerIntro = ({ startGame }) => {
 
   return (
     <div className="intro-section mb-20">
-      <div className="intro-header text-center mb-5">
+      <div className="intro-header text-center mb-8">
         <h2 className="text-2xl sm:text-3xl font-display font-bold mb-2">What is Fermi Poker?</h2>
         <p className="text-medium-brown dark:text-golden-light">Fermi Poker is like poker, but instead of cards, players make guesses about numerical questions.</p>
       </div>
