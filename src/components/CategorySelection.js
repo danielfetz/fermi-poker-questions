c8eedff50b99487cd83d80da68c9743f6330600a
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-03-23 17:17:43 +0100

    Update CategorySelection.js

--- src/components/CategorySelection.js
+++ src/components/CategorySelection.js
@@ -72,17 +72,6 @@ const CategorySelection = ({ questionSets }) => {
                 </svg>
                 <span>{totalQuestionCount} questions</span>
               </div>
-              
-          /* Subcategories count - maybe add in the future
-              {hasSubcategories && (
-                <div className="flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1 text-golden-accent" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
-                  </svg>
-                  <span>{category.subcategories.length} subcategories</span>
-                </div>
-              )}
-          */
             </div>
           </div>
           
