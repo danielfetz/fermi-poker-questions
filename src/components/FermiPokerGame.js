6cbdf23d7974d72b67eff80487012e220bcbf750
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-03-26 12:08:34 +0100

    Update FermiPokerGame.js

--- src/components/FermiPokerGame.js
+++ src/components/FermiPokerGame.js
@@ -362,7 +362,7 @@ const FermiPokerGame = ({ questionSets, darkMode }) => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
-            Back to Categories
+            Back to categories
           </button>
         </div>
         
