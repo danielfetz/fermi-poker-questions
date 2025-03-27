7d0ebf52a501f89f514aabc8fe88582aad78b221
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-03-27 16:33:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ const App = () => {
               aria-label="Settings"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
+                <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
               </svg>
             </button>
             
@@ -97,8 +97,6 @@ const App = () => {
                 className="settings-menu absolute right-0 top-full mt-2 rounded-lg shadow-xl z-50"
               >
                 <div className="settings-menu-content p-3">
-                  <h3 className="font-display font-bold text-lg mb-2 pb-2 border-b">Settings</h3>
-                  
                   {/* Theme toggle option */}
                   <div className="flex justify-between items-center py-2">
                     <span>Theme</span>
