c3dad4634daa7566e60f0c11a61fb21905bce560
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-03-27 17:13:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -86,9 +86,9 @@ const App = () => {
               aria-label="Settings"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <circle cx="10" cy="5" r="1.5" />
+                <circle cx="5" cy="10" r="1.5" />
                 <circle cx="10" cy="10" r="1.5" />
-                <circle cx="10" cy="15" r="1.5" />
+                <circle cx="15" cy="10" r="1.5" />
               </svg>
             </button>
             
