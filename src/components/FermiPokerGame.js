da8ea2aa00db41f84aeb195fef7d27fdda31d0d0
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-03-28 18:47:21 +0100

    Update FermiPokerGame.js

--- src/components/FermiPokerGame.js
+++ src/components/FermiPokerGame.js
@@ -289,8 +289,8 @@ const FermiPokerGame = ({ questionSets, darkMode }) => {
             aria-label="Show game rules"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M9 9a3 3 0 1 1 4 2.8 1 1 0 0 0-1 1V14"></path>
-              <circle cx="12" cy="17" r="1"></circle>
+              <path d="M8.5 10.5A3.5 3.5 0 0 1 12 7a3.5 3.5 0 0 1 3.5 3.5c0 1.5-1 2-1.5 3-0.5 1-0.5 2-0.5 2"></path>
+              <circle cx="12" cy="19" r="0.7"></circle>
             </svg>
           </button>
         </div>
